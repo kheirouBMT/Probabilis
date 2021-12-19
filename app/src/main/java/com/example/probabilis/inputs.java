2852488c880880442a3549d0d2828fdc37e09953
PAK <boumatragk@gmail.com>
2021-12-19 16:42:11 +0100

    test

---
+++ app/src/main/java/com/example/probabilis/inputs.java
@@ -0,0 +1,14 @@
+package com.example.probabilis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class inputs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inputs);
+    }
+}
